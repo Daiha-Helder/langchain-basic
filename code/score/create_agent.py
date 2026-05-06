3db9cc5c2ba36b0d9baef7286320e1361d0ffa70
Daiha-Helder <daihahelder@gmail.com>
2026-05-06 09:51:48 -0300

    create-basic-agent

---
+++ code/score/create_agent.py
@@ -0,0 +1,54 @@
+import argparse
+from langchain_openai import ChatOpenAI
+from langchain_ollama import ChatOllama
+from langchain.agents import create_agent
+from langchain.tools import tool
+from langchain_core.messages import HumanMessage
+from dotenv import load_dotenv
+load_dotenv()
+
+parser = argparse.ArgumentParser(
+    description="Rodar o script com parâmetros configuráveis."
+)
+parser.add_argument("--provider", type=str)
+
+args = parser.parse_args()
+provider = args.provider
+
+@tool
+def search(query: str) -> str:
+    """
+    Tool that searches over internet 
+    Args:
+        query: The query to search for 
+    Returns:
+        The search result
+    """
+    print(f"Searching for {query}")
+    return "Tokyo weather is sunny"
+
+
+if provider == "openai":
+    llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo")
+elif provider == "ollama":
+    llm = ChatOllama(temperature=0, model="gemma4:e4b")
+else:
+    raise ValueError(
+        "Provider not supported. Use --provider openai or --provider ollama."
+    )
+
+tools = [search]
+agent = create_agent(
+    model=llm,
+    tools=tools
+)
+
+def main():
+    print("Hello from langchain-course!")
+    result = agent.invoke({
+        "messages": HumanMessage(content="What is the weather in Tokyo?")
+    })
+    print(result)
+
+if __name__ == "__main__":
+    main()
